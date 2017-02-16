c027f29a65489ed480ec1888cd4bf31f6cec39cb
Rémi Prévost <remi@exomel.com>
2017-02-16 06:09:02 -0500

    Zip response parts

--- lib/plug_letsencrypt.ex
+++ lib/plug_letsencrypt.ex
@@ -38,16 +38,18 @@ defmodule PlugLetsEncrypt do
   """
   @spec init(opts) :: opts
   def init(opts) do
-    opts
+    [key, secret] = opts
     |> Keyword.fetch!(:response)
     |> String.split(".")
+
+    Enum.zip([:key, :secret], [key, secret])
   end
 
   @doc """
   Call the plug.
   """
   @spec call(%Conn{}, opts) :: %Conn{}
-  def call(conn = %Conn{request_path: request_path, method: method}, [key, secret])
+  def call(conn = %Conn{request_path: request_path, method: method}, key: key, secret: secret)
     when method == @method and request_path == @path <> key do
     conn
     |> send_resp(@success_code, "#{key}.#{secret}")
